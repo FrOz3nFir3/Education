a594cd7430665ccf7dbf13f157184844382c637e
Kush Surti <kushsurti05@gmail.com>
2021-05-15 19:45:49 +0530

    adding resize event listener for compiler button (at cost of performance as event will occur a lot)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,34 @@
 var compiler = document.querySelector("div.compiler");
 var div = document.querySelector("div.flex");
 
+// on smaller screen sizes adding the button
 if (window.innerWidth <= 768) {
-  compiler.style.display = "none";
-  let button = document.createElement("button");
-  button.innerText = "Compiler";
-  button.style = "position:fixed; right:0; top:350px; padding:1em";
-  button.className = "bg-green-400";
-  div.append(button);
+  addOrRemoveCompilerButton();
+}
+
+// this code will work, when someone scrolls (either using inspect element or resizes the browser window)
+window.addEventListener("resize", addOrRemoveCompilerButton);
+
+function addOrRemoveCompilerButton() {
+  // on smaller screenSizes adding button and hiding the div.
+  if (window.innerWidth <= 768) {
+    compiler.style.display = "none";
+    if (!buttonAlreadyExists()) {
+      let button = document.createElement("button");
+      button.innerText = "Compiler";
+      button.style = "position:fixed; right:0; top:350px; padding:1em";
+      button.className = "bg-green-400";
+      div.append(button);
+    }
+  } else {
+    // removing the button and displaying back the compiler (div)
+    compiler.style.display = "block";
+    let button = buttonAlreadyExists();
+    button.remove();
+  }
+}
+// resizing event will occur a lot, meaning a lot of creation and duplication of buttons, that's why checking if button is
+//already in the dom.
+function buttonAlreadyExists() {
+  return document.querySelector("button");
 }
