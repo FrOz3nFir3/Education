1ffd6f56e278ca209e66fbcfc6746d32b16ef0b5
vrokx <vaibhavraval9819@gmail.com>
2021-06-23 19:19:35 +0530

    refactoring content page

---
+++ js/html.js
@@ -0,0 +1,15 @@
+const htmlCode = `
+<div><div></div>
+
+<div><ul class="prev-next">
+    <li><a class="button minimal" href="/en-US/docs/Learn/Getting_started_with_the_web/Dealing_with_files"> Previous </a></li>
+    <li><a class="button minimal" href="/en-US/docs/Learn/Getting_started_with_the_web"> Overview: Getting started with the web</a></li>
+    <li><a class="button minimal" href="/en-US/docs/Learn/Getting_started_with_the_web/CSS_basics"> Next  </a></li>
+</ul></div>
+
+<div class="summary">
+<p>HTML (<strong>H</strong>yper<strong>t</strong>ext <strong>M</strong>arkup <strong>L</strong>anguage) is the code that is used to structure a web page and its content. For example,&nbsp;content could be structured within a set of paragraphs,&nbsp;a list of bulleted points, or using images and data tables.&nbsp;As the title suggests, this article will give you a basic understanding of HTML and its functions.</p>
+</div></div>
+`
+
+export default htmlCode;
